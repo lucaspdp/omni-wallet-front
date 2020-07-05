a28645c4c6da9f25c6894d23c80c538d300ffd9a
Lucas Dias <48134977+lucaspdp@users.noreply.github.com>
2020-07-05 19:49:50 -0300

    Update Incoming Calendar

--- src/pages/Dashboard/fragments/IncomeTimelineFragment/components/Calendar.tsx
+++ src/pages/Dashboard/fragments/IncomeTimelineFragment/components/Calendar.tsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import React from 'react';
 
-export default function Calendar(props) {
+import { Container } from './CalendarStyle';
+
+type PropsTypes = {
+  gains : Array<any>,
+  days ? : number,
+}
+
+type DayGainType = {
+  day : number,
+  gain : number,
+  type : string,
+  nome ? : string,
+  setor ? : string,
+  qtd ? : number,
+  prazo ? : number,
+  prazoFunc ? : number;
+  formaPg ? : string,
+  aPagar ? : number,
+  qtdFunc ? : number,
+  valLiquido ? : number,
+  operadora ? : string,
+  antecipacao ? : string,
+  valBruto ? : number,
+  taxa ? : number,
+  periodo ? : string,
+  regraRepasse ? : string
+}
+
+export default function Calendar(props : PropsTypes) {
+  console.log(props);
+
+  type DaysArrayType = {
+    day : number,
+    month : string
+  }
+
+  var maxDays = 35;
+  var daysInMonth = props.days ? props.days : 30;
+  var daysArray : DaysArrayType[] = [];
+
+  for(var i=1; i<=daysInMonth; i++){
+    daysArray.push({
+      month: "this",
+      day: i
+    });
+  }
+
+  for(var i=1; i<= maxDays-daysInMonth; i++){
+    daysArray.push({
+      month: "next",
+      day: i
+    });
+  }
+
+  function moneyFormat(val : number){
+    return val.toLocaleString('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    })
+  }
+
+  function generateGain(gain : DayGainType){
+    return(
+      <div className={`schedule ${gain.type}`}>
+        <span>{moneyFormat(gain.gain)}</span>
+      </div>
+    )
+  }
+
   return (
-    <h1>Hello World</h1>
+    <Container>
+      <div className="daysOfWeek">
+        <span>DOM</span>
+        <span>SEG</span>
+        <span>TER</span>
+        <span>QUA</span>
+        <span>QUI</span>
+        <span>SEX</span>
+        <span>SAB</span>
+      </div>
+      <ul>
+        {daysArray.map(day=>(
+          <li key={day.month + day.day}>
+            <span className={`day ${day.month !='this' && 'other'}`}>{day.day}</span>
+            {props.gains.map(gain=>{
+              if(gain.day == day.day && day.month == 'this'){
+                return generateGain(gain)
+              }
+            })}
+          </li>
+        ))}
+      </ul>
+    </Container>
   )
 }
