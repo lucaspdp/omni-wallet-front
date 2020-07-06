da2b6dad39b148ad7a5ae007502c2e32c8d59660
Lucas Dias <48134977+lucaspdp@users.noreply.github.com>
2020-07-05 21:12:49 -0300

    Update Simulation

--- src/pages/Dashboard/fragments/SimulationFragment/index.tsx
+++ src/pages/Dashboard/fragments/SimulationFragment/index.tsx
@@ -1,5 +1,172 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { 
+  Container,
+  PaymentContainer,
+  CompareButton
+} from './styles';
+
+import Cielo from '../../../../assets/img/paymentMethods/cielo.svg';
+import Stone from '../../../../assets/img/paymentMethods/stone.png';
+import Redecard from '../../../../assets/img/paymentMethods/redecard.png';
+
+import Paypal from '../../../../assets/img/paymentMethods/paypal.svg';
+import Pagseguro from '../../../../assets/img/paymentMethods/pagseguro.png';
+
+import iFood from '../../../../assets/img/marketplaces/ifood.png';
+import Rappi from '../../../../assets/img/marketplaces/rappi.png';
+import Americanas from '../../../../assets/img/marketplaces/americanas.png';
+
+import { random } from 'faker';
 
 export default function SimulationFragment() {
-  return (<div>Simulações</div>);
+
+  const [paymentMethodOffline, setPaymentMethodOffline] = useState('Cielo');
+  const [paymentMethodOnline, setPaymentMethodOnline] = useState('Cielo');
+  const [Marketplace, setMarketplace] = useState('iFood');
+
+  const paymentMethodsOffline = [{
+    name: 'Cielo',
+    image: Cielo,
+    taxa: '15,5 % por transação + R$100,00 mensais (Plano Básico sem entregador)',
+    pagamento: 'Divide o valor do repasse por semana, pagando sempre as quarta-feira, 4 semanas após a venda'
+  },{
+    name: 'Stone',
+    image: Stone,
+    taxa: '15,5 % por transação + R$100,00 mensais (Plano Básico sem entregador)',
+    pagamento: 'Divide o valor do repasse por semana, pagando sempre as quarta-feira, 4 semanas após a venda'
+  },{
+    name: 'Redecard',
+    image: Redecard,
+    taxa: '3,49 % em pagamentos o a vista e 3,49% em pagamentos parcelados',
+    pagamento: 'Quando paga: Divide o valor do repasse por semana, pagando sempre as quarta-feira, 4 semanas após a venda'
+  }];
+
+  const paymentMethodsOnline = [{
+    name: 'Cielo',
+    image: Cielo,
+    taxa: '15,5 % por transação + R$100,00 mensais (Plano Básico sem entregador)',
+    pagamento: 'Divide o valor do repasse por semana, pagando sempre as quarta-feira, 4 semanas após a venda'
+  },{
+    name: 'Paypal',
+    image: Paypal,
+    taxa: '14,79% mais R$ 0,60 fixo por transações à vista. Vendas parceladas a taxa é de 1,92%.',
+    pagamento: 'Repasse de todas as compras é feito em até 24 horas após a venda'
+  },{
+    name: 'Pagseguro',
+    image: Pagseguro,
+    taxa: '4,49 + 0,40 de adesão e 2,99% mensais para habilitar pagamento parcelado',
+    pagamento: '14 ou 31 dias após a venda'
+  }];
+
+  const MarketplacesMethods = [{
+    name: 'iFood',
+    image: iFood,
+    taxa: '15,5 % por transação + R$100,00 mensais (Plano Básico sem entregador)',
+    pagamento: 'Divide o valor do repasse por semana, pagando sempre as quarta-feira, 4 semanas após a venda'
+  },{
+    name: 'Rappi',
+    image: Rappi,
+    taxa: '3,5% por transação',
+    pagamento: 'Vendas da 1ª quinzena caem dia 8 do mês seguinte, da 2ª quinzena caem dia 23 do mês seguinte'
+  },{
+    name: 'Americanas',
+    image: Americanas,
+    taxa: '16% por transação',
+    pagamento: 'Vendas da 1ª quinzena caem dia 5 do mês seguinte, da 2ª quinzena caem dia 20 do mês seguinte'
+  }];
+
+  return (
+    <Container>
+      <PaymentContainer>
+        <span className="type">
+          Pagamentos - loja física: 
+          <span className="detail">Compare o custo benefício de diferentes operadores de pagamentos</span>
+        </span>
+        <div className="paymentMethods">
+          {paymentMethodsOffline.map(method=>(
+            <div className="paymentCard" id={method.name + random.number(100)} >
+              <img src={method.image} alt={method.name}/>
+              <span className="sub">
+                Taxa: <span className="desc">{method.taxa}</span>
+              </span>
+              <span className="sub">
+                Quando paga: <span className="desc">{method.pagamento}</span>
+              </span>
+              <span className={`now ${method.name == paymentMethodOffline && 'active'}`}>
+                Utilizando atualmente
+              </span>
+              <button type="button">
+                Simular uso
+              </button>
+
+            </div>
+          ))}
+        </div>
+        <CompareButton>
+          Comparar
+        </CompareButton>
+      </PaymentContainer>
+      
+      <PaymentContainer>
+        <span className="type">
+          Pagamentos - loja online: 
+          <span className="detail">Compare o custo benefício de diferentes operadores de pagamentos</span>
+        </span>
+        <div className="paymentMethods">
+          {paymentMethodsOnline.map(method=>(
+            <div className="paymentCard" id={method.name + random.number(100)} >
+              <img src={method.image} alt={method.name}/>
+              <span className="sub">
+                Taxa: <span className="desc">{method.taxa}</span>
+              </span>
+              <span className="sub">
+                Quando paga: <span className="desc">{method.pagamento}</span>
+              </span>
+              <span className={`now ${method.name == paymentMethodOnline && 'active'}`}>
+                Utilizando atualmente
+              </span>
+              <button type="button">
+                Simular uso
+              </button>
+
+            </div>
+          ))}
+        </div>
+        <CompareButton>
+          Comparar
+        </CompareButton>
+      </PaymentContainer>
+            
+      <PaymentContainer>
+        <span className="type">
+          Market Places: 
+          <span className="detail">Compare o custo benefício dos principais marketplaces do mercado</span>
+        </span>
+        <div className="paymentMethods">
+          {MarketplacesMethods.map(method=>(
+            <div className="paymentCard" id={method.name + random.number(100)} >
+              <img src={method.image} alt={method.name}/>
+              <span className="sub">
+                Taxa: <span className="desc">{method.taxa}</span>
+              </span>
+              <span className="sub">
+                Quando paga: <span className="desc">{method.pagamento}</span>
+              </span>
+              <span className={`now ${method.name == Marketplace && 'active'}`}>
+                Utilizando atualmente
+              </span>
+              <button type="button">
+                Simular uso
+              </button>
+
+            </div>
+          ))}
+        </div>
+        <CompareButton>
+          Comparar
+        </CompareButton>
+      </PaymentContainer>
+    </Container>
+  );
 }
---
+++ src/pages/Dashboard/fragments/SimulationFragment/styles.tsx
@@ -0,0 +1,125 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  width: 100%;
+  height: 100%;
+  max-height: 100%;
+  background: #fff;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
+export const PaymentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding-top: 30px;
+  padding-left: 50px;
+  padding-right: 50px;
+  padding-bottom: 20px;
+
+  span.type{
+    font-weight: 600;
+    font-size: 14;
+    margin-bottom: 10px;
+    .detail{
+      font-weight: 400;
+    }
+  }
+
+  div.paymentMethods{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+
+    .paymentCard{
+      display: flex;
+      flex-direction: column;
+      width: 27%;
+      height: 350px;
+      min-width: 300px;
+      box-shadow: 0px 0px 10px #00000047;
+      border-radius: 8px;
+      border: 1px solid transparent;
+      transition: border-color 0.2s;
+
+      &:hover{
+        border-color: var(--primary-color);
+      }
+
+      justify-content: center;
+      padding: 0 20px;
+
+
+      img{
+        margin: 0 auto;
+        width: 120px;
+        height: 60px;
+        object-fit: scale-down;
+        margin-bottom: 20px;
+        margin-top: 10px;
+      }
+
+      span.sub{
+        font-weight: 800;
+        font-size: 16px;
+        margin-top: 10px;
+
+        .desc{
+          margin-left: 5px;
+          font-weight: 200;
+        }
+      }
+
+      .now{
+        margin-top: 10px;
+        margin: 0 auto;
+        color: transparent;
+        margin-top: 30px;
+
+        &.active{
+          color: #656565;
+        }
+      }
+
+      button{
+        margin: 0 auto;
+        background: transparent;
+        cursor: pointer;
+        color: var(--primary-color);
+        border: 1px solid transparent;
+        transition: border-bottom-color 0.2s;
+        font-size: 14px;
+        margin-top: auto;
+        margin-bottom: 30px;
+        width: 80px;
+
+        &:hover{
+          border-bottom-color: var(--primary-color);
+        }
+      }
+    }
+  }
+`;
+
+export const CompareButton = styled.button`
+
+  background: var(--primary-color);
+  border-radius: 29px;
+  width: 180px;
+  height: 40px;
+  cursor: pointer;
+
+  margin: 30px auto;
+
+  color: white;
+  font-weight: bolder;
+  font-size: 16px;
+
+  transition: transform 0.2s;
+
+  &:hover{
+    transform: scale(1.05);
+  }
+
+`;
